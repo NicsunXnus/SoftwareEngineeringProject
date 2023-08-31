aa50bb90fe38884ea63b5c35240b060d7e2fa764
Cyolune <jan.alfenson.tan@gmail.com>
2023-09-01 01:09:38 +0800

    Create tokenizerfunctions class

---
+++ Team00/Code00/source/TokenizerClasses/TokenizerFunctions.h
@@ -0,0 +1,19 @@
+#ifndef TOKENIZERFUNCTIONS_H
+#define TOKENIZERFUNCTIONS_H
+
+#include <string>
+#include <string_view>
+#include <vector>
+#include "../TokenClasses/Token.h"
+
+using namespace std::string_view_literals;
+
+/**
+* This class represents the common functions that both PQL Tokenizer and SP Tokenizer could use
+*/
+class TokenizerFunctions {
+public:
+	TokenizerFunctions() {};
+};
+
+#endif
