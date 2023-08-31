56f7be1ee3df5415d67fff12ba9f920b238d0d11
Cyolune <jan.alfenson.tan@gmail.com>
2023-09-01 02:43:05 +0800

    To pull

--- Team00/Code00/source/TokenizerClasses/PQLTokenizer.cpp
+++ Team00/Code00/source/TokenizerClasses/PQLTokenizer.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <string.h>
 #include "PQLTokenizer.h"
 
 using namespace Tokens;
@@ -8,9 +9,21 @@ using namespace Tokens;
 * Returns a std::vector of tokens.
 */
 std::vector<Token> tokenize(std::string_view pql) {
+
 	std::vector<std::shared_ptr<Token>> tokens;
+	int startIndex{ 0 };
+	bool isWord{ false };
 
+	for (int i = 0; i < pql.length(); ++i) {
+		if (pql[i] == ';') {
+			tokens.push_back(std::make_shared<SemicolonSepToken>());
+			continue;
+		}
 
+		if (pql[i] == ',') {
+			tokens.push_back(std::make_shared<)
+		}
+	}
 
 }
 
