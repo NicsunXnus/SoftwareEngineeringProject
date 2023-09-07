e695e11f8dc54ef9c9893db148ec199055c267fa
FestiveCat <31hanyang@gmail.com>
2023-09-08 02:38:36 +0800

    Add Tokeniser Integration

--- Team18/Code18/AutoTester/source/TestWrapper.cpp
+++ Team18/Code18/AutoTester/source/TestWrapper.cpp
@@ -1,4 +1,7 @@
+#include <fstream>
+
 #include "TestWrapper.h"
+#include "TokenizerClasses/SIMPLETokenizer.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -17,15 +20,25 @@ TestWrapper::TestWrapper() {
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
-	// call your parser to do the parsing
+  // call your parser to do the parsing
+  std::ifstream simpleStream(filename);
+
+  std::string src = "";
+  if (simpleStream.is_open()) {
+    while (simpleStream) {
+      src += simpleStream.get();
+    }
+  }
+  std::vector<std::vector<std::shared_ptr<Token>>> tokens = SimpleTokenizer::tokenize(src);
   // ...rest of your code...
+
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
+void TestWrapper::evaluate(std::string query, std::list<std::string>& results) {
+  // call your evaluator to evaluate the query here
+    // ...code to evaluate query...
 
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+    // store the answers to the query in the results list (it is initially empty)
+    // each result must be a string.
 }
