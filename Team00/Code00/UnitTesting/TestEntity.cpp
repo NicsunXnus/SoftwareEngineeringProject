8b0e55f322a5e513b386dc27df38b70582a1fcad
ong-hongkai <hong_kai_ong_from.tp@mas.gov.sg>
2023-09-07 15:45:43 +0800

    Fix compilation errors

--- Team00/Code00/UnitTesting/TestEntity.cpp
+++ Team00/Code00/UnitTesting/TestEntity.cpp
@@ -6,26 +6,27 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTesting
 {
-	TEST_CLASS(TestSP)
+	TEST_CLASS(TestEntity)
 	{
 	public:
 		TEST_METHOD(TestProcedureEntity)
 		{
             //Create procedure nodes
-            std::shared_ptr<ProcedureNode> testProcdeureNode1 = std::make_shared<ProcedureNode>("procedure1")
-			std::shared_ptr<ProcedureNode> testProcdeureNode2 = std::make_shared<ProcedureNode>("procedure2")
-            std::shared_ptr<ProcedureNode> testProcdeureNode3 = std::make_shared<ProcedureNode>("procedure3")
-            std::shared_ptr<ProcedureNode> testProcdeureNode4 = std::make_shared<ProcedureNode>("procedure4")
-            std::shared_ptr<ProcedureNode> testProcdeureNode5 = std::make_shared<ProcedureNode>("procedure5")
-            
-            //Combine procedure nodes into a vector
-            std::vector<std::shared_ptr<ProcedureNode>> testProcedureNodes = {testProcdeureNode1, testProcdeureNode2, testProcdeureNode3, testProcdeureNode4, testProcdeureNode5};
-            
-            //Create procedure entity
+            std::shared_ptr<ProcedureNode> testProcdeureNode1 = std::make_shared<ProcedureNode>("procedure1");
+            std::shared_ptr<ProcedureNode> testProcdeureNode2 = std::make_shared<ProcedureNode>("procedure2");
+            std::shared_ptr<ProcedureNode> testProcdeureNode3 = std::make_shared<ProcedureNode>("procedure3");
+            std::shared_ptr<ProcedureNode> testProcdeureNode4 = std::make_shared<ProcedureNode>("procedure4");
+            std::shared_ptr<ProcedureNode>testProcdeureNode5 = std::make_shared<ProcedureNode>("procedure5");
+
+            // Combine procedure nodes into a vector
+            std::vector<std::shared_ptr<ProcedureNode>>
+                testProcedureNodes = {testProcdeureNode1, testProcdeureNode2, testProcdeureNode3, testProcdeureNode4, testProcdeureNode5};
+
+            // Create procedure entity
             std::shared_ptr<ProcedureEntity> testProcedureEntity = std::make_shared<ProcedureEntity>();
 
-            //Extract procedure entity from each procedure node
-            for (const auto& procedure : testProcedureNodes) {
+            // Extract procedure entity from each procedure node
+            for (const auto &procedure : testProcedureNodes) {
                 testProcedureEntity->extractEntity(procedure);
             }
 
