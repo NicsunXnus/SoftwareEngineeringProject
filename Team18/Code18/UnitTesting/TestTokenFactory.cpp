f650700bf531517e3e07f2dbf4d01562a8ca0553
FestiveCat <31hanyang@gmail.com>
2023-09-08 16:16:46 +0800

    add tests and equality checks for tokens

--- Team18/Code18/UnitTesting/TestTokenFactory.cpp
+++ Team18/Code18/UnitTesting/TestTokenFactory.cpp
@@ -60,21 +60,58 @@ namespace UnitTesting
 
 	TEST_CLASS(Test_TokenFactory_generateToken) {
 	private:
+		// Some variables to make the code cleaner in the test methods
 		std::string tokenName = "";
-		bool forSimple = "";
-		bool forceIdentifier = "";
+		bool forSimple = NULL;
+		bool forceIdentifier = NULL;
 		std::shared_ptr<Token> (*genToken) (std::string, bool, bool) = TokenFactory::generateToken;
 
 	public:
-		TEST_METHOD(CHANGE_THIS_SUCCESS) {
+		TEST_METHOD(integerLiterals_success) {
+			// lambda function that tests for all combinations of forSimple and forceIdentifier
+			auto miniWrapper = [&](std::string tokenName) {
+				std::shared_ptr<Token> result_simple_nonIden = genToken(tokenName, forSimple, forceIdentifier);
+				std::shared_ptr<Token> result_pql_nonIden = genToken(tokenName, !forSimple, forceIdentifier);
+				std::shared_ptr<Token> result_simple_iden = genToken(tokenName, forSimple, !forceIdentifier);
+				std::shared_ptr<Token> result_pql_iden = genToken(tokenName, !forSimple, !forceIdentifier);
+
+				std::shared_ptr<IntegerLiteralToken> expected = std::make_shared<IntegerLiteralToken>(tokenName);
+				bool isEqual = Token::checkEquality(result_simple_nonIden, expected);
+				isEqual = isEqual && Token::checkEquality(result_pql_nonIden, expected);
+				isEqual = isEqual && Token::checkEquality(result_simple_iden, expected);
+				isEqual = isEqual && Token::checkEquality(result_pql_iden, expected);
+				return isEqual;
+			};
 			tokenName = "0";
-			forSimple = "CHANGE_THIS";
-			forceIdentifier = "CHANGE_THIS";
-			//std::shared_ptr<Token> result = genToken(tokenName, forSimple, forceIdentifier);
-			std::shared_ptr<IntegerLiteralToken> result = std::make_shared<IntegerLiteralToken>("1");
-			std::shared_ptr<IntegerLiteralToken> expected = std::make_shared<IntegerLiteralToken>(tokenName);
-			bool isEqual = Token::checkEquality(result, expected);
+			forSimple = false;
+			forceIdentifier = false;
+			bool isEqual = miniWrapper(tokenName);
 			assert(isEqual == true); // success
+
+			tokenName = "123";
+			bool isEqual = miniWrapper(tokenName);
+			assert(isEqual = true);
+		}
+		TEST_METHOD(underscore_success) {
+
+		}
+		TEST_METHOD(operators_success) {
+
+		}
+		TEST_METHOD(separators_success) {
+
+		}
+		TEST_METHOD(simpleKeywords_success) {
+
+		}
+		TEST_METHOD(pqlKeywords_success) {
+
+		}
+		TEST_METHOD(commonKeywords_success) {
+
+		}
+		TEST_METHOD(forceIdentifier_sucess) {
+
 		}
 	};
 }
--- Team18/Code18/source/TokenClasses/Token.h
+++ Team18/Code18/source/TokenClasses/Token.h
@@ -21,6 +21,7 @@ public:
 		return this->tokenName;
 	}
 
+	// Checks if the two tokens supplied are the same runtime type and same stored tokenName.
 	static bool checkEquality(Token* left, Token* right) {
 		std::string leftId = typeid(*left).name();
 		std::string rightId = typeid(*right).name();
@@ -33,6 +34,7 @@ public:
 		return leftName == rightName;
 	}
 
+	// Overloaded method to take in shared_ptr instead of regular pointers
 	static bool checkEquality(std::shared_ptr<Token> left, std::shared_ptr<Token> right) {
 		return checkEquality(left.get(), right.get());
 	}
