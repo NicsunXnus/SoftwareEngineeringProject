5c86f474924848784880fdfcdf35b94245132628
FestiveCat <31hanyang@gmail.com>
2023-09-10 14:41:39 +0800

    add skeleton for integration tests

--- Team18/Code18/IntegrationTesting/TestFrontEnd.cpp
+++ Team18/Code18/IntegrationTesting/TestFrontEnd.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "../AutoTester/source/TestWrapper.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,9 +10,16 @@ namespace IntegrationTesting
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(DESCRIBE_THIS_TEST)
 		{
-			// TODO: Your test code here
+			std::string srcCode = "";
+			TestWrapper testWrapper;
+			testWrapper.directParse(srcCode);
+			std::string query = "";
+			std::list<std::string> output;
+			testWrapper.evaluate(query, output);
+			std::list<std::string> expected;
+			assert(output == expected);
 		}
 
 	};
