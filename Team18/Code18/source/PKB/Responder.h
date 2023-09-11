01ff058ad132460df7e715207b0309d9441f8908
marcuschj <haojie.chua@gmail.com>
2023-09-11 19:58:31 +0800

    Test commit

--- Team18/Code18/source/PKB/Responder.h
+++ Team18/Code18/source/PKB/Responder.h
@@ -14,7 +14,7 @@ using namespace std;
 class Responder {
 public:
 	// methods to respond to queries. generally, call the storage manager to do work (static)
-	// storage manager will get entity storage/abstraction storage, perform the query on that class via polymorphism, then return line numbers
+	// storage manager will get entity storage/abstraction storage, perform the query on that class via polymorphism, then return line numbers.
 	// responder returns line numbers to caller (QPS)
 	
 	vector<string> Responder::getEntityStatement(ENTITY entity) const {
