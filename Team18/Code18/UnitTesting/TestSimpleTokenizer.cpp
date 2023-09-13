2e760b7b2b5a5dafcf04468b453a0672bc1e8a2b
FestiveCat <31hanyang@gmail.com>
2023-09-13 19:32:58 +0800

    add unit test

--- Team18/Code18/UnitTesting/TestSimpleTokenizer.cpp
+++ Team18/Code18/UnitTesting/TestSimpleTokenizer.cpp
@@ -2,40 +2,107 @@
 #include "CppUnitTest.h"
 
 #include "../source/TokenizerClasses/SimpleTokenizer.h"
+#include "../source/ExceptionMessages.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace SimpleTokeniser_Test
 {
 	TEST_CLASS(tokeniseMethod_Test) {
-	private:
-		bool equalityWrapper(std::vector<std::vector<std::shared_ptr<Token>>> first,
-			std::vector<std::vector<std::shared_ptr<Token>>> second) {
-			if (!&first || !&second) { // If either are nulls
-				return false;
-			}
-			bool output = true;
-			int firstSize = first.size();
-			// Check for first dimension same size
-			output = output && firstSize == second.size();
-			// if no first dimension or not same size, return output
-			if (!output || firstSize == 0) return output;
-			for (int stmtNum = 0; stmtNum < firstSize; stmtNum++) {
-				std::vector<std::shared_ptr<Token>> firstStmt = first[stmtNum];
-				std::vector<std::shared_ptr<Token>> secondStmt = second[stmtNum];
-				int firstSize = firstStmt.size();
-				// Check for second dimension same size
-				output = output && firstSize == secondStmt.size();
-				// if no second dimension or not same size, return output
-				if (!output || firstSize == 0) return output;
-				for (int tokenNum = 0; tokenNum < firstSize; tokenNum++) {
-					std::shared_ptr<Token> firstToken = firstStmt[tokenNum];
-					std::shared_ptr<Token> secondToken = secondStmt[tokenNum];
-					output = output && Token::checkEquality(firstToken, secondToken);
-				}
-			}
-			return output;
-		}
 	public:
+		TEST_METHOD(emptyProgram_failure) {
+			try {
+				std::string input = "";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::emptyProgramGiven);
+			}
+		}
+
+		TEST_METHOD(emptyProcedure_failure) {
+			try {
+				std::string input = "procedure x {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::emptyProcedureGiven);
+			}
+		}
+
+		TEST_METHOD(invalidProcedureName_failure) {
+			try {
+				std::string input = "procedure 1 {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+			try {
+				std::string input = "procedure 0x1 {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+			try {
+				std::string input = "procedure ' {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+		}
+
+		TEST_METHOD(invalidProcedureDeclaration_failure) {
+			try {
+				std::string input = "pr0cedure x {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+			try {
+				std::string input = "Procedure x {}"; // capitals
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+			try {
+				std::string input = "procedure    {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+			try {
+				std::string input = "     procName   {}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::invalidProcedureDefinition);
+			}
+		}
+
+		TEST_METHOD(emptyStatement_failure) {
+			try {
+				std::string input = "procedure x { ;}";
+				SimpleTokenizer::tokenizeProgram(input);
+				assert(false);
+			}
+			catch (std::invalid_argument e) {
+				assert(e.what() == ExceptionMessages::emptyProcedureGiven);
+			}
+		}
 	};
 }
