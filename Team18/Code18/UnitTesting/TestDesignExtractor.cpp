afe0a4d0b8fbaac195c1c14e1a7762513c214cac
ong-hongkai <hong_kai_ong_from.tp@mas.gov.sg>
2023-09-15 05:26:20 +0800

    tests

--- Team18/Code18/UnitTesting/TestDesignExtractor.cpp
+++ Team18/Code18/UnitTesting/TestDesignExtractor.cpp
@@ -294,14 +294,14 @@ namespace UnitTesting
                   std::shared_ptr<ProcedureEntity> testProcedureEntity = testDesignExtractor.getProcedureEntity();
 
                   // Create UsesAbstraction  without shared_ptr
-                  // UsesAbstraction testUsesAbstraction = UsesAbstraction();
-                  // std::shared_ptr<ModifiesAbstraction> testModifiesAbstraction = std::make_shared<ModifiesAbstraction>();
+                  UsesAbstraction testUsesAbstraction = UsesAbstraction();
+                  std::shared_ptr<ModifiesAbstraction> testModifiesAbstraction = std::make_shared<ModifiesAbstraction>();
 
-                  // // Get shared pointer to procedure map
-                  // std::shared_ptr<std::map<std::string, std::vector<std::string>>> procedureMap = testProcedureEntity->getMap();
+                  // Get shared pointer to procedure map
+                  std::shared_ptr<std::map<std::string, std::vector<std::string>>> procedureMap = testProcedureEntity->getMap();
 
-                  // // Extract Uses and Modifies from procedure1
-                  // testUsesAbstraction.extractUsesAbstraction(testProgramNode);
+                  // Extract Uses and Modifies from procedure1
+                  testUsesAbstraction.extractUsesAbstraction(testProgramNode);
                   // testModifiesAbstraction->extractModifiesAbstraction(testProgramNode);
                   
                   // // Get UsesMap and ModifiesMap from UsesAbstraction and ModifiesAbstraction by using getUsesStorageMap and getModifiesStorageMap
