5dba40b4eae343ff843a78e01b2305b350b59c8b
marcuschj <haojie.chua@gmail.com>
2023-09-14 21:54:01 +0800

    Set up test files

---
+++ Team18/Code18/UnitTesting/TestPKBResponder.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <PKB.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting {
+	TEST_CLASS(TestPKBResponder) {
+		public:
+			TEST_METHOD(TestQueryEntities) {
+				
+			}
+		};
+}
