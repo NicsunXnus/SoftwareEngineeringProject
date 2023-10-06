f9a61e0c540503abadd41eddc8df34fa5c048f59
ong-hongkai <hong_kai_ong_from.tp@mas.gov.sg>
2023-10-07 03:17:00 +0800

    Change logic for Next

--- Team18/Code18/source/DesignExtractor/NextAbstractionExtractor.h
+++ Team18/Code18/source/DesignExtractor/NextAbstractionExtractor.h
@@ -16,9 +16,9 @@ using namespace std;
 class NextAbstractionExtractor : public AbstractionExtractor {
 public:    
     // Constructor
-    NextAbstractionExtractor() {
-        this->ifStorageMap = std::make_shared<map<string, vector<string>>>();
-    }
+    // NextAbstractionExtractor() {
+    //     this->ifStorageMap = std::make_shared<map<string, vector<string>>>();
+    // }
 
     void handleAssign(std::shared_ptr<AssignNode> assignNode) override {}
     void handleCall(std::shared_ptr<CallNode> callNode) override {}
@@ -33,59 +33,70 @@ public:
     }
 
     void handleWhile(std::shared_ptr<WhileNode> whileNode) override {
-        std::vector<std::shared_ptr<StatementNode>> statements = whileNode->getStatements();
-        string whileStatementNumber = to_string(whileNode->getStatementNumber());
+        // std::vector<std::shared_ptr<StatementNode>> statements = whileNode->getStatements();
+        // string whileStatementNumber = to_string(whileNode->getStatementNumber());
         
-        // Connect the first statement number to the while statement number
-        string firstStatementNumber = to_string(statements.front()->getStatementNumber());
-        insertToAbstractionMap(whileStatementNumber, firstStatementNumber);
+        // // Connect the first statement number to the while statement number
+        // string firstStatementNumber = to_string(statements.front()->getStatementNumber());
+        // insertToAbstractionMap(whileStatementNumber, firstStatementNumber);
         
-        string lastStatment = traverse(statements);
-        insertToAbstractionMap(lastStatment, whileStatementNumber);
+        // string lastStatment = traverse(statements);
+        // insertToAbstractionMap(lastStatment, whileStatementNumber);
     }
 
     void handleIf(std::shared_ptr<IfNode> ifNode) override {
-        std::vector<std::shared_ptr<StatementNode>> ifStatements = ifNode->getStatements();
-        std::vector<std::shared_ptr<StatementNode>> elseStatements = ifNode->getElseStatements();
+        // std::vector<std::shared_ptr<StatementNode>> ifStatements = ifNode->getStatements();
+        // std::vector<std::shared_ptr<StatementNode>> elseStatements = ifNode->getElseStatements();
 
-        string ifStatementNumber = to_string(ifNode->getStatementNumber());
-        // Get first value of ifStatements
-        string firstIfStatementNumber = to_string(ifStatements.front()->getStatementNumber());
-        // Get first value of elseStatements
-        string firstElseStatementNumber = to_string(elseStatements.front()->getStatementNumber());
+        // string ifStatementNumber = to_string(ifNode->getStatementNumber());
+        // // Get first value of ifStatements
+        // string firstIfStatementNumber = to_string(ifStatements.front()->getStatementNumber());
+        // // Get first value of elseStatements
+        // string firstElseStatementNumber = to_string(elseStatements.front()->getStatementNumber());
 
-        insertToAbstractionMap(ifStatementNumber, firstIfStatementNumber);
-        insertToAbstractionMap(ifStatementNumber, firstElseStatementNumber);
+        // insertToAbstractionMap(ifStatementNumber, firstIfStatementNumber);
+        // insertToAbstractionMap(ifStatementNumber, firstElseStatementNumber);
 
-        string endIfStatementNumber = traverse(ifStatements);
-        string endElseStatementNumber = traverse(elseStatements);
+        // string endIfStatementNumber = traverse(ifStatements);
+        // string endElseStatementNumber = traverse(elseStatements);
 
-        insertToIfStorageMap(ifStatementNumber, endIfStatementNumber, endElseStatementNumber);
+        // insertToIfStorageMap(ifStatementNumber, endIfStatementNumber, endElseStatementNumber);
     }
 
     void extractAbstractions(shared_ptr<ASTNode> astNode) override {
         extractDesigns(astNode);
-        processIfLines();
+        // processIfLines();
     }
 
 private:
-    std::shared_ptr<map<string, vector<string>>> ifStorageMap;
+    // std::shared_ptr<map<string, vector<string>>> ifStorageMap;
 
     string traverse(std::vector<std::shared_ptr<StatementNode>> statements) {
-        unordered_set<string> prevStatementNumbers = {};
+        string prevStatementNumber = "";
         for (const auto& statement : statements) {
             string statementNumber = to_string(statement->getStatementNumber());
-            if (prevStatementNumbers.size() > 0) {
-                for (const auto& prevStatement : prevStatementNumbers) {
-                    insertToAbstractionMap(prevStatement, statementNumber);
-                }
-                prevStatementNumbers.clear();
+            if (prevStatementNumber != "") {
+                insertToAbstractionMap(prevStatementNumber, statementNumber);
             }
-            prevStatementNumbers.insert(statementNumber);
-            extractDesigns(statement);
-            // if its the last statement, return the statement number to caller
-            if (statement == statements.back() && statement->getName() != "if") {
-                insertKeyToAbstractionMap(statementNumber);
+
+            if (statement->getName() != "if") {
+                prevStatementNumbers.insert(statementNumber);
+            }
+
+            if (statement->getName() == "while") {
+                // Get the last statement number of the while statement
+                string lastStatementNumber = traverse(statement->getStatements());
+                insertToAbstractionMap(lastStatementNumber, statementNumber);
+
+            } else if (statement->getName() == "if") {
+                // Get the last statement number of the if statement
+                string lastStatementNumber = traverse(statement->getStatements());
+                insertToAbstractionMap(lastStatementNumber, statementNumber);
+
+                // Get the last statement number of the else statement
+                lastStatementNumber = traverse(statement->getElseStatements());
+                insertToAbstractionMap(lastStatementNumber, statementNumber);
+            } else if (statement == statements.back() && statement->getName() != "if" && statement->getName() != "while") {
                 return statementNumber;
             }
         }
@@ -93,31 +104,31 @@ private:
 
 
     // Replace the values of the keys for IF statemetns in the abstraction map with the values in the ifStorageMap
-    void processIfLines() {
-        for (const auto& [key, value] : *ifStorageMap) {
-            if (AbstractionStorageMap->find(key) != AbstractionStorageMap->end()) {
-                AbstractionStorageMap->at(key) = value;
-            }
-        }
-    }
+    // void processIfLines() {
+    //     for (const auto& [key, value] : *ifStorageMap) {
+    //         if (AbstractionStorageMap->find(key) != AbstractionStorageMap->end()) {
+    //             AbstractionStorageMap->at(key) = value;
+    //         }
+    //     }
+    // }
 
-    void insertKeyToAbstractionMap(string key) {
-        if (this->AbstractionStorageMap->find(key) == this->AbstractionStorageMap->end()) {
-            this->AbstractionStorageMap->insert({ key, vector<string>() });
-        }
-    }
+    // void insertKeyToAbstractionMap(string key) {
+    //     if (this->AbstractionStorageMap->find(key) == this->AbstractionStorageMap->end()) {
+    //         this->AbstractionStorageMap->insert({ key, vector<string>() });
+    //     }
+    // }
 
-    void insertToIfStorageMap(string key, string lastIfLineNumber, string lastElseLineNumber) {
-        if (this->ifStorageMap->find(key) == this->ifStorageMap->end()) {
-            this->ifStorageMap->insert({ key, vector<string>() });
-        }
-        if (std::find(this->ifStorageMap->at(key).begin(), this->ifStorageMap->at(key).end(), lastIfLineNumber) == this->ifStorageMap->at(key).end()) {
-            this->ifStorageMap->at(key).push_back(lastIfLineNumber);
-        }
-        if (std::find(this->ifStorageMap->at(key).begin(), this->ifStorageMap->at(key).end(), lastElseLineNumber) == this->ifStorageMap->at(key).end()) {
-            this->ifStorageMap->at(key).push_back(lastElseLineNumber);
-        }
-    }
+    // void insertToIfStorageMap(string key, string lastIfLineNumber, string lastElseLineNumber) {
+    //     if (this->ifStorageMap->find(key) == this->ifStorageMap->end()) {
+    //         this->ifStorageMap->insert({ key, vector<string>() });
+    //     }
+    //     if (std::find(this->ifStorageMap->at(key).begin(), this->ifStorageMap->at(key).end(), lastIfLineNumber) == this->ifStorageMap->at(key).end()) {
+    //         this->ifStorageMap->at(key).push_back(lastIfLineNumber);
+    //     }
+    //     if (std::find(this->ifStorageMap->at(key).begin(), this->ifStorageMap->at(key).end(), lastElseLineNumber) == this->ifStorageMap->at(key).end()) {
+    //         this->ifStorageMap->at(key).push_back(lastElseLineNumber);
+    //     }
+    // }
 
 
 
