2309a90418bad2c4f95a983bf9313ef655010722
ryantianj <ryantianjun@gmail.com>
2023-11-03 17:21:20 +0800

    abstract constants

---
+++ Team18/Code18/source/Constants/QPSConstants.h
@@ -0,0 +1,11 @@
+#ifndef QPSCONSTANTS_H
+#define QPSCONSTANTS_H
+
+#include <string>
+
+using namespace std;
+
+static inline const string TRUE_STRING = "TRUE";
+static inline const string FALSE_STRING = "FALSE";
+
+#endif
