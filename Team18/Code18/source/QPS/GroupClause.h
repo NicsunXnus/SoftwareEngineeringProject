526519ca5590b9b5f05fba02ad608544044d8d2c
Nicholas Sun <e0543645@u.nus.edu>
2023-11-03 21:16:55 +0800

    Split GroupClause into header and source

--- Team18/Code18/source/QPS/GroupClause.h
+++ Team18/Code18/source/QPS/GroupClause.h
@@ -7,79 +7,30 @@ class GroupClause {
 public:
     GroupClause() {}
 
-    void addOne(shared_ptr< QueryResultsTable> table) {
-        members.emplace_back(table);
-        updateHeaders(table->getHeaders());
-    }
+    void addOne(shared_ptr< QueryResultsTable> table);
 
-    void addMany(vector<shared_ptr< QueryResultsTable>> tables) {
-        for (shared_ptr< QueryResultsTable> table : tables)
-            addOne(table);
-    }
+    void addMany(vector<shared_ptr< QueryResultsTable>> tables);
 
-    void merge(shared_ptr < GroupClause > _group) {
-        addMany(_group->getMembers());
-    }
+    vector<shared_ptr< QueryResultsTable>> getMembers();
 
-    void setMembers(vector<shared_ptr< QueryResultsTable>> tables) {
-        members.clear();
-        addMany(tables);
-    }
+    set<string> getHeaders();
 
-    void reduceToOne() {
-        if (size() == 0) return;
-        shared_ptr<QueryResultsTable> intermediateTable = members[0];
-        members.erase(members.begin());
-        shared_ptr<QueryResultsTable> currTable;
-        for (shared_ptr<QueryResultsTable> table : members) {
-            currTable = table;
-            // for empty but significant tables
-            if (currTable->isEmpty()) {
-                if (currTable->getSignificant()) {
-                    continue; // just keep current table
-                }
-                else { // no need to evaluate the rest of the query  
-                    members = { QueryResultsTable::createEmptyTable() };
-                    return;
-                }
-            }
-            if (intermediateTable->haveSimilarHeaders(currTable)) {
-                //do inner join
-                intermediateTable = intermediateTable->innerJoin(currTable);
-            }
-            else {
-                //do cross product
-                intermediateTable = intermediateTable->crossProduct(currTable);
-            }
-        }
-        members = { intermediateTable };
-    }
+    bool hasCommonHeaders(shared_ptr < GroupClause > _group);
 
-    int size() {
-        return members.size();
-    }
+    bool hasEmptyTablesFirst();
 
-    vector<shared_ptr< QueryResultsTable>> getMembers() {
-        return members;
-    }
-    set<string> getHeaders() {
-        return headers;
-    }
-    bool hasCommonHeaders(shared_ptr < GroupClause > _group) {
-        set<string> otherGroupHeaders = _group->getHeaders();
-        set<string> intersect;
-        set_intersection(otherGroupHeaders.begin(), otherGroupHeaders.end(), headers.begin(), headers.end(), inserter(intersect, intersect.begin()));
-        return intersect.size() > 0;
-    }
+    void merge(shared_ptr < GroupClause > _group);
 
-    bool hasEmptyTablesFirst() {
-        return !members.empty() && members[0]->isEmpty();
-    }
+    void reduceToOne();
+
+    void setMembers(vector<shared_ptr< QueryResultsTable>> tables);
+
+    int size();
+
 private: 
-    void updateHeaders(vector<string> _headers) {
-        set_union(headers.cbegin(), headers.cend(), _headers.cbegin(), _headers.cend(), inserter(headers, headers.begin()));
-    }
     vector<shared_ptr< QueryResultsTable>> members;
     set<string> headers;
+
+    void updateHeaders(vector<string> _headers);
 };
 #endif
