f176655fcb2c2eccf468ad5731f98c5110aa328d
Cyolune <jan.alfenson.tan@gmail.com>
2023-11-12 00:20:24 +0800

    Test attrRef getSynonyms

--- Team18/Code18/UnitTesting/TestQueryObject.cpp
+++ Team18/Code18/UnitTesting/TestQueryObject.cpp
@@ -74,5 +74,14 @@ namespace UnitTesting
 			Assert::IsTrue(1 == static_cast<int>(stmtS.getSynonyms()->size()));
 		}
 
+		TEST_METHOD(TestAttrRefObject)
+		{	
+			shared_ptr<SynonymObject> synA = make_shared<SynonymObject>(SynonymObject("a", ASSIGN));
+			shared_ptr<ClauseArg> synArgA = make_shared<ClauseArg>(ClauseArg("a", synA));
+
+			StmtNoObject stmtS = StmtNoObject("a"sv, synArgA);
+
+			Assert::IsTrue(1 == static_cast<int>(stmtS.getSynonyms()->size()));
+		}
 	};
 }
